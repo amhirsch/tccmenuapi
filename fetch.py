f50a603bb0a2457272c2c7c66af1e80bb921e8ef
= <=>
2019-12-06 23:35:52 -0800

    Filters only the daily specials from the menu parse
    Method signatures still remain unclear.
    Docstrings for extraction could be better

--- fetch.py
+++ fetch.py
@@ -1,4 +1,5 @@
 import pprint
+from typing import Any, Dict
 
 from bs4 import BeautifulSoup
 import requests
@@ -13,12 +14,21 @@ HALL_URL = {
     COLLINS: 'https://collins-cmc.cafebonappetit.com/cafe/collins/',
 }
 
-def extract_meals(raw_html):
+def extract_meals(entire_page: BeautifulSoup) -> Dict[str, Any]:
     meals = {}
-    meals[BREAKFAST] = raw_html.find('section', {'id': 'breakfast'})
-    meals[LUNCH] = raw_html.find('section', {'id': 'lunch'})
-    meals[DINNER] = raw_html.find('section', {'id': 'dinner'})
+    breakfast_raw = entire_page.find('section', {'id': 'breakfast'})
+    lunch_raw = entire_page.find('section', {'id': 'lunch'})
+    dinner_raw = entire_page.find('section', {'id': 'dinner'})
+    meals[BREAKFAST] = extract_specials(breakfast_raw)
+    meals[LUNCH] = extract_specials(lunch_raw)
+    meals[DINNER] = extract_specials(dinner_raw)
     return meals
+
+def extract_specials(meal):
+    # the specials will be listed first, so find() is acceptable
+    special_section = meal.find('div', {'class': 'site-panel__daypart-tab-content'})
+    # there is a single usable child, so extract this specifically
+    return special_section.find('div', {'class': 'site-panel__daypart-tab-content-inner'})
 
 def extract_stations(raw_html):
     stations = {}
@@ -28,7 +38,7 @@ def extract_stations(raw_html):
         stations[name] = station
     return stations
 
-def extract_specials(raw_html):
+def extract_foods(raw_html):
     all_specials = raw_html.find_all('button', {'class': 'h4 site-panel__daypart-item-title'})
     return list(map((lambda x: x.get_text(strip=True)), all_specials))
 
@@ -37,5 +47,7 @@ if __name__ == "__main__":
     soup = None
     if r.status_code == 200:
         soup = BeautifulSoup(r.text, 'html.parser')
+        meals = extract_meals(soup)
+        dinner = meals[DINNER]
     else:
         "connection did not work"
