ece6cc0da4ec06c09dd462874ab3d36b61b81839
= <=>
2019-12-13 12:47:34 -0800

    Utilized a mapping for css classes
    A Python dictionary controls access to the CSS classes used by Cafe
    Bon Appetit.
    There are a few modifications to the HTML tags, but that will be
    formalized in a later commit.

--- fetch_bonappetit.py
+++ fetch_bonappetit.py
@@ -4,6 +4,8 @@ from typing import Any, Dict, List, Tuple, Union
 from bs4 import BeautifulSoup
 import requests
 
+import html_tags #TODO move variables over
+
 BREAKFAST = 'Breakfast'
 LUNCH = 'Lunch'
 DINNER = 'Dinner'
@@ -13,6 +15,29 @@ COLLINS = 'Collins'
 HALL_URL = {
     COLLINS: 'https://collins-cmc.cafebonappetit.com/cafe/collins/',
 }
+
+SECTION = 'section'
+DIV = 'div'
+BUTTON = 'buuton'
+H2 = 'h2'
+H3 = 'h3'
+
+CSS_CLASS = {
+    'meal_name': 'site-panel__daypart-panel-title',
+    'meal_time': 'site-panel__daypart-time',
+    'meal_menu': 'site-panel--daypart',
+    'section_tab': 'site-panel__daypart-tab-content-inner',
+    'food_item': 'site-pannel__daypart-item',
+    'food_container': 'site-panel__daypart-item-container',
+    'food_title': 'h4 site-panel__daypart-item-title',
+    'food_description': 'site-panel__daypart-item-description',
+    'station_block': 'station-title-inline-block',
+    'note_tag': 'site-panel__diet-pref-row',
+    'note_title': 'site-panel__diet-pref-header-inner',
+    'note_description': 'site-panel__diet-pref-acc-content'
+
+}
+
 
 def extract_meals(entire_page: BeautifulSoup) -> Dict[str, Tuple[str, Any]]:
     """Extracts all the meals for the day.
@@ -26,13 +51,13 @@ def extract_meals(entire_page: BeautifulSoup) -> Dict[str, Tuple[str, Any]]:
         of the meal times and a BeautifulSoup tag for the daily specials.
     """
     meals = {}
-    all_meals = entire_page.find_all('section', class_='site-panel--daypart')
+    all_meals = entire_page.find_all(SECTION, class_=CSS_CLASS['meal_menu'])
 
     for meal in all_meals:
-        name = meal.find('h2', class_='site-panel__daypart-panel-title').get_text(strip=True)
-        time = meal.find('div', class_='site-panel__daypart-time').get_text(strip=True)
+        name = meal.find(H2, class_=CSS_CLASS['meal_name']).get_text(strip=True)
+        time = meal.find(DIV, class_=CSS_CLASS['meal_time']).get_text(strip=True)
         # the specials will be listed first, so no need to check other tabs
-        specials = meal.find('div', class_='site-panel__daypart-tab-content-inner')
+        specials = meal.find(DIV, class_=CSS_CLASS['section_tab'])
 
         meals[name] = (time, specials)
     
@@ -51,17 +76,17 @@ def extract_stations(daily_special) -> Dict[str, Any]:
             an 'other' station.
     """
     stations = {'other': []}
-    current_div = daily_special.find('div')
+    current_div = daily_special.find(DIV)
 
     while current_div is not None:
         if current_div == '\n':
             pass
         else:
-            if 'site-pannel__daypart-item' in current_div.attrs['class']:
+            if CSS_CLASS['food_item'] in current_div.attrs['class']:
                 # a standalone item
-                stations['other'] += [ current_div.find('button') ]
-            elif 'station-title-inline-block' in current_div.attrs['class']:
-                name = current_div.find('h3').get_text()
+                stations['other'] += [ current_div.find(BUTTON) ]
+            elif CSS_CLASS['station_block'] in current_div.attrs['class']:
+                name = current_div.find(H3).get_text()
                 stations[name] = current_div
         
         current_div = current_div.next_sibling
@@ -82,7 +107,7 @@ def extract_food_containers(anc_tag) -> List[Any]:
     Returns:
         A list of BeautifulSoup tags of food items.
     """
-    all_buttons = anc_tag.find_all('div', {'class': 'site-panel__daypart-item-container'})
+    all_buttons = anc_tag.find_all(DIV, class_=CSS_CLASS['food_container'])
     return list(map( (lambda x: x), all_buttons ))
 
 
@@ -90,7 +115,8 @@ def extract_food_header(food_tag) -> Any:
     """Isolates the food header tag. This contains information such as food
         title and dietary notes.
     """
-    return food_tag.find('button', {'class': 'h4 site-panel__daypart-item-title'})
+    # TODO
+    return food_tag.find('button', {'class': CSS_CLASS['food_title']})
 
 
 def extract_food_title(food_tag) -> str:
@@ -114,7 +140,7 @@ def extract_food_notes(food_tag) -> List[str]:
 
 def extract_food_description(raw_html) -> str:
     """Isolates the description for a given food"""
-    desc_div = raw_html.find('div', {'class': 'site-panel__daypart-item-description'})
+    desc_div = raw_html.find('div', {'class': CSS_CLASS['food_description']})
     #TODO decide how to deal with multi line arguments
 
     if desc_div is None:
@@ -169,11 +195,11 @@ def extract_note_mapping(entire_page):
         A dictionary whose keys are note descriptions and values are note titles. 
     """
     note_map = {}
-    note_tag = entire_page.find_all('div', class_='site-panel__diet-pref-row')
+    note_tag = entire_page.find_all('div', class_=CSS_CLASS['note_tag'])
 
     for tag in note_tag:
-        title = tag.find('span', class_='site-panel__diet-pref-header-inner').get_text(strip=True)
-        description = tag.find('div', class_='site-panel__diet-pref-acc-content').get_text(strip=True)
+        title = tag.find('span', class_=CSS_CLASS['note_title']).get_text(strip=True)
+        description = tag.find('div', class_=CSS_CLASS['note_description']).get_text(strip=True)
         note_map[description] = title
     
     return note_map
