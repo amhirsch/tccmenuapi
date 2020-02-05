c0d87468d0593d956a232ab00db9f192a3782231
Xander Hirsch <ahirsch@hmc.edu>
2020-02-05 12:05:15 -0800

    fixed line length problems

--- scrapers/bonappetit.py
+++ scrapers/bonappetit.py
@@ -40,7 +40,8 @@ def extract_meals(entire_page: BeautifulSoup) -> Dict[str, Tuple[str, Any]]:
     """Extracts all the meals for the day.
 
     Args:
-        entire_page: A BeautifulSoupo representation of the dining hall index.html
+        entire_page: A BeautifulSoupo representation of the dining hall
+            index.html
 
     Returns:
         A dictionary whose keys are the meal names.
@@ -48,11 +49,14 @@ def extract_meals(entire_page: BeautifulSoup) -> Dict[str, Tuple[str, Any]]:
         of the meal times and a BeautifulSoup tag for the daily specials.
     """
     meals = {}
-    all_meals = entire_page.find_all(const.SECTION, class_=CSS_CLASS['meal_menu'])
+    all_meals = entire_page.find_all(const.SECTION,
+                                     class_=CSS_CLASS['meal_menu'])
 
     for meal in all_meals:
-        name = meal.find(const.H2, class_=CSS_CLASS['meal_name']).get_text(strip=True)
-        time = meal.find(const.DIV, class_=CSS_CLASS['meal_time']).get_text(strip=True)
+        name = meal.find(const.H2,
+                         class_=CSS_CLASS['meal_name']).get_text(strip=True)
+        time = meal.find(const.DIV,
+                         class_=CSS_CLASS['meal_time']).get_text(strip=True)
         # the specials will be listed first, so no need to check other tabs
         specials = meal.find(const.DIV, class_=CSS_CLASS['section_tab'])
 
@@ -69,8 +73,8 @@ def extract_stations(daily_special) -> Dict[str, Any]:
 
     Returns:
         A dictionary whose keys are the serving stations for the dining hall.
-        Note that any food item listed outside a station will be encapsulated into
-            an 'other' station.
+        Note that any food item listed outside a station will be encapsulated
+            into an 'other' station.
     """
     stations = {const.OTHER: []}
     current_div = daily_special.find(const.DIV)
@@ -104,7 +108,8 @@ def extract_food_containers(anc_tag) -> List[Any]:
     Returns:
         A list of BeautifulSoup tags of food items.
     """
-    all_buttons = anc_tag.find_all(const.DIV, class_=CSS_CLASS['food_container'])
+    all_buttons = anc_tag.find_all(const.DIV,
+                                   class_=CSS_CLASS['food_container'])
     return list(map((lambda x: x), all_buttons))
 
 
@@ -152,8 +157,8 @@ def generate_food_data(food_tag, note_map) -> Dict[str, Union[str, List[str]]]:
     Args:
         food_tag: A BeautifulSoup tag containing the food item.
         note_map: A dictionary whose keys are the note descriptions (found in)
-            the food_tag content and values are the quick reference for the note.
-            See extract_note_mapping()
+            the food_tag content and values are the quick reference for the
+            note. See extract_note_mapping()
 
     Returns:
         A dictionary with the following keys:
@@ -181,22 +186,25 @@ def extract_note_mapping(entire_page):
     """Generates a mapping between the food note and the note key.
 
     The Cafe Bon Appetit menu html has food tags whose notes are represented as
-        images. The text is the longer description, which is cumbersome to read.
-        This function creates a mapping between the longer description, provided
-        in a food item and a quick name.
+        images. The text is the longer description, which is cumbersome to
+        read. This function creates a mapping between the longer description,
+        provided in a food item and a quick name.
 
     Args:
         entire_page: A BeautifulSoup representation of the menu html page.
 
     Returns:
-        A dictionary whose keys are note descriptions and values are note titles.
+        A dictionary whose keys are note descriptions and values are note
+            titles.
     """
     note_map = {}
     note_tag = entire_page.find_all(const.DIV, class_=CSS_CLASS['note_tag'])
 
     for tag in note_tag:
-        title = tag.find(const.SPAN, class_=CSS_CLASS['note_title']).get_text(strip=True)
-        description = tag.find(const.DIV, class_=CSS_CLASS['note_description']).get_text(strip=True)
+        title = tag.find(const.SPAN,
+                         class_=CSS_CLASS['note_title']).get_text(strip=True)
+        description = tag.find(const.DIV,
+                               class_=CSS_CLASS['note_description']).get_text(strip=True)
         note_map[description] = title
 
     return note_map
@@ -214,7 +222,8 @@ def parse_station_menu(station_tag, notes_map):
         A list whose elements are the food data station's offerings.
         See generate_food_data().
     """
-    return [generate_food_data(x, notes_map) for x in extract_food_containers(station_tag)]
+    return [generate_food_data(x, notes_map)
+            for x in extract_food_containers(station_tag)]
 
 
 def parse_menu(entire_page):
@@ -232,12 +241,14 @@ def parse_menu(entire_page):
     notes_map = extract_note_mapping(entire_page)
 
     for meal in all_meals:
-        meal_time = list(map((lambda x: x.isoformat('minutes')), extract_hours(all_meals[meal][0])))
+        meal_time = list(map((lambda x: x.isoformat('minutes')),
+                             extract_hours(all_meals[meal][0])))
         meal_stations = extract_stations(all_meals[meal][1])
         station_sub_menu = {}
 
         for station in meal_stations:
-            station_sub_menu[station] = parse_station_menu(meal_stations[station], notes_map)
+            station_sub_menu[station] = parse_station_menu(
+                                            meal_stations[station], notes_map)
 
         menu[meal] = {const.HOURS: meal_time, const.SPECIALS: station_sub_menu}
 
@@ -249,7 +260,8 @@ def extract_hours(time):
     Returns a list of opening and closing times"""
     time_list = time.split(' - ')
 
-    return list(map (lambda x: dt.datetime.strptime(x, '%I:%M %p').time()), time_list))
+    return list(map (lambda x: dt.datetime.strptime(x, '%I:%M %p').time()),
+                time_list))
 
 
 if __name__ == "__main__":
